4a9dc8f04b56e5d47a7d44c2674e119fda05f6aa
dazehinn <dazehinn@yahoo.fr>
2022-12-21 17:59:04 +0100

    Update javascript file

--- script.js
+++ script.js
@@ -1,48 +1,47 @@
-const btn = document.querySelector('.hamburger');
+const navItems = document.querySelector('.nav-items');
+const navLinks = document.querySelectorAll('.nav-items a');
+const hamburgerBtn = document.querySelector('.hamburger');
 
-const navbar = document.querySelector('nav');
-
-const navlink = document.querySelectorAll('.nav-items a');
-const hamburgerImage = document.querySelector('.hamburger-img');
+const closeBtn = document.createElement('img');
+closeBtn.src = 'img/toolbar.png';
+closeBtn.style.position = 'fixed';
+closeBtn.style.top = '25px';
+closeBtn.style.right = '10px';
+closeBtn.style.pointer = 'cursor';
 
 function openMenu() {
-  if (navbar.style.height === 'initial') {
-    navbar.style.height = '100%';
-  } else {
-    navbar.style.height = 'initial';
-  }
-  if (navbar.style.backgroundColor === 'transparent') {
-    navbar.style.backgroundColor = '#396df2';
-  } else {
-    navbar.style.backgroundColor = 'transparent';
-  }
+  navItems.style.display = 'block';
+  navItems.style.width = '100%';
+  navItems.style.height = '100%';
+  navItems.style.backgroundColor = '#396df2';
+  navItems.style.opacity = 0.9;
+  navItems.style.zIndex = 9;
+  navItems.style.position = 'fixed';
+  navItems.style.top = 0;
+  navItems.style.left = 0;
+  navItems.style.padding = '24px';
+  hamburgerBtn.style.display = 'none';
+  navItems.appendChild(closeBtn);
+  closeBtn.style.display = 'initial';
 
-  for (let i = 0; i < navlink.length; i += 1) {
-    if (navlink[i].style.display === 'none') {
-      navlink[i].style.display = 'block';
-    } else {
-      navlink[i].style.display = 'none';
-    }
-  }
-
-  if (hamburgerImage.src === 'img/enabled.png') {
-    hamburgerImage.src = 'img/Toolbar.png';
-  } else {
-    hamburgerImage.src = 'img/toolbar.png';
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].style.display = 'block';
   }
 }
 
 function closeMenu() {
-  navlink[0].style.display = 'none';
-  navlink[1].style.display = 'none';
-  navlink[2].style.display = 'none';
-  hamburgerImage.src = 'img/Enabled.png';
-  navbar.style.height = 'initial';
-  navbar.style.backgroundColor = 'transparent';
+  closeBtn.style.display = 'none';
+  hamburgerBtn.style.display = 'initial';
+  navItems.style.width = '0%';
+  navItems.style.height = '0%';
+  navItems.style.backgroundColor = 'transparent';
+  for (let i = 0; i < navLinks.length; i += 1) {
+    navLinks[i].style.display = 'none';
+  }
 }
 
-btn.addEventListener('click', openMenu, false);
-
-navlink[0].addEventListener('click', closeMenu);
-navlink[1].addEventListener('click', closeMenu);
-navlink[2].addEventListener('click', closeMenu);
+hamburgerBtn.addEventListener('click', openMenu);
+navLinks[0].addEventListener('click', closeMenu);
+navLinks[1].addEventListener('click', closeMenu);
+navLinks[2].addEventListener('click', closeMenu);
+closeBtn.addEventListener('click', closeMenu);
