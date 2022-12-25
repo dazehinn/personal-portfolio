40620fc4070cbce7f0843660e0e91953ee3eea4c
dazehinn <dazehinn@yahoo.fr>
2022-12-25 01:39:33 +0100

    Update script file.

--- script.js
+++ script.js
@@ -45,3 +45,16 @@ navLinks[0].addEventListener('click', closeMenu);
 navLinks[1].addEventListener('click', closeMenu);
 navLinks[2].addEventListener('click', closeMenu);
 closeBtn.addEventListener('click', closeMenu);
+
+const errorMessage = document.querySelector('.error-message');
+const form = document.querySelector('.contact-form');
+const email = document.querySelector('#user-email');
+const emailRegex = /^[a-z0-9_.]+@[a-z0-9_.]+\.[a-z0-9_.]+$/;
+
+form.addEventListener('submit', (e) => {
+  if (!emailRegex.test(email.value)) {
+    e.preventDefault();
+    errorMessage.style.visibility = 'visible';
+    email.style.border = '2px solid #dd5353';
+  }
+});
